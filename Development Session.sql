f39de2ed47ddef5784e6f74f35cd9ddc15ee55c2
ISMAILAWA <ismailawa.aliyu@gmail.com>
2019-09-01 14:52:13 +0100

    form working now

---
+++ Development Session.sql
@@ -0,0 +1,10 @@
+INSERT INTO
+  `nafecmlss`.`salutations` (active, created_at, id, name, updated_at)
+VALUES
+  (
+    '1',
+    'created_at:timestamp',
+    id :int,
+    'MR',
+    'updated_at:timestamp'
+  );
